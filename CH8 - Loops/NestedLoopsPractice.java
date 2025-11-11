a64cd04c09fc20a235f897e55c023def0af1d21e
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-11-11 17:07:18 +0000

    timeout lol

---
+++ CH8 - Loops/NestedLoopsPractice.java
@@ -0,0 +1,113 @@
+public class NestedLoopsPractice {
+    public static void main(String[] args) {
+        Practice1();
+        System.out.println(" ");
+        Practice2();
+        System.out.println(" ");
+        Practice3();
+        System.out.println(" ");
+        Practice4();
+        System.out.println(" ");
+        Practice5();
+        System.out.println(" ");
+        Practice6();
+        System.out.println(" ");
+        Practice7();
+        System.out.println(" ");
+        Practice8();
+        System.out.println(" ");
+        Practice9();
+    }
+
+    public static void Practice1() {
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 2; col++) {
+                System.out.print(1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice2() {
+        String word = "A";
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 4; col++) {
+                System.out.print(word + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice3() {
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col < 4; col++) {
+                System.out.print(row * col);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice4() {
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col < 5; col++) {
+                System.out.print(row * col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice5() {
+        for (int row = 0; row <= 4; row++) {
+            for (int col = 2; col <= 5; col++) {
+                System.out.print(row + col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice6() {
+        for (int row = 18; row <= 23; row++) {
+            for (int col = 2; col <= 7; col++) {
+                System.out.print(row + col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice7() {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice8() {
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 5; col >= row; col--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Practice9() {
+        int as = 5;
+        int bs = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            for (int a = 0; a < as; a++) {
+                System.out.println("A");
+            }
+            for (int b = 0; b < bs ; b++) {
+                System.out.println("B");
+            }
+
+            as--;
+            bs++;
+            System.out.println();
+        }
+    }
+}
